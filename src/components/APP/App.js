2c03630395c547c2f4ceae709a019e294e9f75ee
DoveLai <laiyu0603@foxmail.com>
2017-09-07 01:25:13 +0000

    first commit

--- src/components/APP/App.js
+++ src/components/APP/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import demo from '../components/Demo';
-
+import demo from '../Demo';
+import './App.less';
 // const sss = React
 
 export default React.createClass({
-    defaultProps: {};
-
     
-
-    this.route.push({
-        ''
-    });
 
     render() {
         let t = '';
         
         return (
             <div>
-                <demo></demo>
-                <section>
-                  {this.props.children || 'Welcome to React kick off'}
+                <section className="b-detail">
+                  
+                  <div className="head">借款详情</div>
+                  <ul className="detail">
+                      <li>
+                          <span className="left">姓名</span>
+                          <span className="right">夏＊＊</span>
+                      </li><li>
+                          <span className="left">身份证</span>
+                          <span className="right">588176*＊＊＊＊＊＊＊1823</span>
+                      </li><li>
+                          <span className="left">借款金额</span>
+                          <span className="right">5000.00元</span>
+                      </li><li>
+                          <span className="left">借款期限</span>
+                          <span className="right">30天</span>
+                      </li><li>
+                          <span className="left">还款方式</span>
+                          <span className="right">到期一次还本付息</span>
+                      </li><li>
+                          <span className="left">还款本息</span>
+                          <span className="right">65000.00</span>
+                      </li>
+
+                  </ul>
                 </section>
             </div>
         );
